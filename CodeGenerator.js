04a8953ba44f7f788720b772d70637591d65d6aa
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-08-14 16:37:57 -0400

    Add generate emails button

--- CodeGenerator.js
+++ CodeGenerator.js
@@ -24,6 +24,37 @@ let generateCode = function(){
     window.open('mailto:test@example.com?subject="Certificate"&body='+encodeURIComponent(output));
 }
 
+let generateEmailButtons = function(){
+    let emailText = "";
+    let inserts = $("inserts").value.split("\n");
+    for (let i = 0; i < inserts.length; i++){
+        let vars = inserts[i].split('\t');
+        let name = vars[0];
+        let email = vars[1];
+        if (!name && !email){
+            continue;
+        }
+        name = name || "(Name)";
+        email = email || "(Email)";
+        emailText += "<br>";
+        emailText += "<div class='btn-email' style='border:1px; border-style:solid;'>";
+            emailText += "<table>";
+                emailText += "<tr>";
+                    emailText += "<td>";
+                        emailText += "<input type='checkbox' id='chkEmail'>";
+                    emailText += "</td>";
+                    emailText += "<td>";
+                        emailText += name;
+                        emailText += "<br>";
+                        emailText += email;
+                    emailText += "</td>";
+                emailText += "</tr>";
+            emailText += "</table>";
+        emailText += "</div>";
+    }
+    $("divEmailButtons").innerHTML = emailText;
+}
+
 //2019-12-27a: copied from https://stackoverflow.com/a/14822579/2336212
 String.prototype.replaceAll = function (find, replace) {
     var str = this;
